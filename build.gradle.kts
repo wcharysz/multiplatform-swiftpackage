84075e6609828b3280eba9ff87369a7d71fb17d4
Luca Spinazzola <mspinluca@gmail.com>
2022-04-27 21:38:01 -0400

    before making an XCFramwork: arm64, x64, and x86 binaries need to be packed in a fat framwork
    https://developer.apple.com/forums/thread/666335

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,9 @@ import java.util.*
 buildscript {
     repositories {
         mavenCentral()
-        jcenter()
     }
     dependencies {
-        classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.2.4")
+        classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.9.0")
     }
 }
 
@@ -21,19 +20,19 @@ plugins {
     signing
 }
 
-version = "2.0.3-arm64"
+version = "2.0.3-arm64-7"
 
 repositories {
-    jcenter()
+    mavenCentral()
 }
 
 dependencies {
-    compileOnly(kotlin("gradle-plugin", "1.5.30"))
-    testImplementation("io.kotest:kotest-runner-junit5:4.3.0")
-    testImplementation("io.kotest:kotest-assertions-core:4.3.0")
-    testImplementation("io.kotest:kotest-property:4.3.0")
-    testImplementation("io.mockk:mockk:1.10.0")
-    testImplementation(kotlin("gradle-plugin", "1.5.30"))
+    compileOnly(kotlin("gradle-plugin", "1.6.21"))
+    testImplementation("io.kotest:kotest-runner-junit5:5.2.3")
+    testImplementation("io.kotest:kotest-assertions-core:5.2.3")
+    testImplementation("io.kotest:kotest-property:5.2.3")
+    testImplementation("io.mockk:mockk:1.12.3")
+    testImplementation(kotlin("gradle-plugin", "1.6.21"))
 }
 
 java {
